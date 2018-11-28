be8008420411d031f15edd4edc036daaa17613d6
tinykite <dakota@tinykitelab.com>
2018-11-27 21:43:00 -0600

    feat: add video randomization
    This commit adds very simple video randomization to Dublab TV. On browser load, a script will add a semi-random index position to the youtube playlist embed url, which will change what video begins playing first.

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,9 @@
-var menuBtn = document.querySelector("#site-nav__button");
-menuBtn.addEventListener('click', toggleSiteMenu);
-
-function toggleSiteMenu() {
-  var toggleValue = document.getElementById("site-nav__button").getAttribute("aria-expanded"); 
-  if (toggleValue == "true") 
-  {
-  toggleValue = "false"
-  } else {
-  toggleValue = "true"
-  }
-  document.getElementById("site-nav__button").setAttribute("aria-expanded", toggleValue);
+function changeUrl() {
+  const url = document.querySelector('.js-trigger');
+	const urlLink = url.getAttribute('src');
+  const randNum = Math.floor(Math.random() * 4);  
+	const newUrl = `${urlLink}&index=${randNum}`;
+	url.src = newUrl;
 }
 
-var searchBtn = document.querySelector("#site-nav__search-toggle");
-searchBtn.addEventListener('click', toggleSearch);
-
-function toggleSearch() {
-  var toggleValue = document.getElementById("site-nav__search-toggle").getAttribute("aria-expanded"); 
-  if (toggleValue == "true") 
-  {
-  toggleValue = "false"
-  } else {
-  toggleValue = "true"
-  }
-  document.getElementById("site-nav__search-toggle").setAttribute("aria-expanded", toggleValue);
-}
+changeUrl();
